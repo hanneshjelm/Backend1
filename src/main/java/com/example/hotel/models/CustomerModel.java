3a9433aaeacf4bd30c566241e4a3c7dcd64f4033
Helenes-Hub <hrydros@gmail.com>
2025-05-15 09:47:45 +0200

    försökt bygga projektstrukturen

---
+++ src/main/java/com/example/hotel/models/CustomerModel.java
@@ -0,0 +1,14 @@
+package com.example.hotel.models;
+
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@Entity
+public class CustomerModel {
+}
