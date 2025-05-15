6dab26bc80bd78dc0ed835974c95e619a67d1381
Helenes-Hub <hrydros@gmail.com>
2025-05-15 10:18:30 +0200

    la till validation / jakarta

--- src/main/java/com/example/hotel/models/CustomerModel.java
+++ src/main/java/com/example/hotel/models/CustomerModel.java
@@ -3,10 +3,13 @@ package com.example.hotel.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 @Data
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
@@ -17,6 +20,14 @@ public class CustomerModel {
     @GeneratedValue
     private Long id;
 
+    @Pattern(regexp="^[A-Öa-ö ]*$", message="only letters for customer name")
+    @Size(min=3, message="atleast 3 characters"
+    )
     private String name;
+
+    @Pattern(
+            regexp = "^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$",
+            message = "Please provide a valid email address"
+    )
     private String email;
 }
