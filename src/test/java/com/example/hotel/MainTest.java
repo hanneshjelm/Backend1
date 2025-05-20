6d5ff57a7974c4383ed4999c4023a94eb68bba0b
Hannes Hjelm <hannes.hjelm1@gmail.com>
2025-05-20 13:33:37 +0200

    La till testmappen som saknades för vi skapade projektet via github

---
+++ src/test/java/com/example/hotel/MainTest.java
@@ -0,0 +1,10 @@
+package com.example.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MainTest {
+    @Test
+    void contextLoads() {}
+}
