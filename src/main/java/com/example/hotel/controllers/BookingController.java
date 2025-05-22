8ad47d13f7b7283befcaf6aa7d499e3f1643581d
Helenes-Hub <hrydros@gmail.com>
2025-05-22 13:36:20 +0200

    börjat på searchByDate (tror det funkar?) behöver fixa med capacity

--- src/main/java/com/example/hotel/controllers/BookingController.java
+++ src/main/java/com/example/hotel/controllers/BookingController.java
@@ -1,20 +1,43 @@
 package com.example.hotel.controllers;
 
 import com.example.hotel.dtos.BookingDetailedDto;
+import com.example.hotel.dtos.BookingDto;
+import com.example.hotel.dtos.RoomDetailedDto;
 import com.example.hotel.services.BookingService;
+import com.example.hotel.services.RoomService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class BookingController {
     private final BookingService bookingService;
+    private final RoomService roomService;
 
     @RequestMapping("bookings")
     public List<BookingDetailedDto> getAllBookings() {
         return bookingService.getAllBookings();
+    }
+
+
+    @GetMapping("/searchAvaliableRooms")
+    public String showSearchForm(Model model) {
+        model.addAttribute("booking", new BookingDto());
+        return "roomSearch";
+    }
+
+    @PostMapping("/searchAvaliableRooms")
+    public String searchAvailableRooms(
+            @ModelAttribute("booking") BookingDto bookingForm,
+            Model model
+    ) {
+        List<RoomDetailedDto> availableRooms = roomService.getAvailableRooms(bookingForm);
+
+        model.addAttribute("rooms", availableRooms);
+        return "avaliableRooms";
     }
 }
--- src/main/java/com/example/hotel/repos/RoomRepository.java
+++ src/main/java/com/example/hotel/repos/RoomRepository.java
@@ -1,7 +1,41 @@
 package com.example.hotel.repos;
 
+import com.example.hotel.dtos.RoomDetailedDto;
 import com.example.hotel.models.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    @Query("""
+        SELECT r FROM Room r
+        WHERE r NOT IN (
+              SELECT b.room FROM Booking b
+              WHERE b.checkInDate < :checkOut
+                AND b.checkOutDate > :checkIn
+          )
+    """)
+    List<Room> findAvailableRooms(
+            @Param("checkIn") LocalDate checkIn,
+            @Param("checkOut") LocalDate checkOut
+           // @Param("guestRequirement") int guestRequirement
+    );
 }
+
+/*
+
+@Query("""
+        SELECT r FROM Room r
+        WHERE r.capacity >= :guestRequirement
+          AND r NOT IN (
+              SELECT b.room FROM Booking b
+              WHERE b.checkInDate < :checkOut
+                AND b.checkOutDate > :checkIn
+          )
+    """)
+
+ */
