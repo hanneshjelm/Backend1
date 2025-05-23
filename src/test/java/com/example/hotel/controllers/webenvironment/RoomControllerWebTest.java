53cefb62f2e9d74650fa706c7c0798500cbe5135
Jennifer Cordova Almonacid <jennifer.cor.alm@outlook.com>
2025-05-24 00:43:36 +0200

    added in MainTest and webEnvironmentTest class

---
+++ src/test/java/com/example/hotel/controllers/webenvironment/RoomControllerWebTest.java
@@ -0,0 +1,25 @@
+package com.example.hotel.controllers.webenvironment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RoomControllerWebTest {
+
+    @Value(value = "${local.server.port}")
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void roomControllerTest() {
+        assertThat(restTemplate.getForObject("http://localhost:" + port + "/rooms/all", String.class))
+                .contains("All Rooms");
+    }
+}
