990fb845f6028670069db8d88749129156886e53
Hannes Hjelm <hannes.hjelm1@gmail.com>
2025-05-26 17:19:17 +0200

    Added thymeleaf for updating customers

--- src/main/java/com/example/hotel/controllers/CustomerController.java
+++ src/main/java/com/example/hotel/controllers/CustomerController.java
@@ -1,10 +1,5 @@
 package com.example.hotel.controllers;
-
-import com.example.hotel.dtos.BookingDetailedDto;
-import com.example.hotel.dtos.BookingDto;
 import com.example.hotel.dtos.CustomerDetailedDto;
-import com.example.hotel.dtos.CustomerDto;
-import com.example.hotel.models.Booking;
 import com.example.hotel.models.Customer;
 import com.example.hotel.repos.CustomerRepository;
 import com.example.hotel.services.BookingService;
@@ -16,9 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/customers")
@@ -83,4 +76,22 @@ public class CustomerController {
         }
         return "redirect:/customers/{id}";
     }
+
+    @GetMapping("/{id}/update")
+    public String updateCustomerForm(@PathVariable Long id, Model model) {
+        Customer customer = customerService.findCustomerById(id);
+        if (customer != null) {
+            CustomerDetailedDto customerDetailedDto = customerService.customerToCustomerDetailedDto(customer);
+            model.addAttribute("customerDetailedDto", customerDetailedDto);
+            return "updateCustomerForm";
+        }
+        return "redirect:/customers/all";
+    }
+
+    @PostMapping("/update")
+    public String updateCustomer(CustomerDetailedDto customerDetailedDto) {
+        customerService.updateCustomer(customerDetailedDto);
+        return "redirect:/customers/all";
+    }
+
 }
